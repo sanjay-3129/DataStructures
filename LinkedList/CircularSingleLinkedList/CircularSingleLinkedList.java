7398ba59ad428cef4d215c1f4cc84e79ce42d8f8
sanjay kumar k p <49227865+sanjay-3129@users.noreply.github.com>
2021-02-27 16:00:59 +0530

    Create CircularSingleLinkedList, slight modification of linkedlist

---
+++ LinkedList/CircularSingleLinkedList/CircularSingleLinkedList.java
@@ -0,0 +1,217 @@
+package com.ds.linkedlist;
+
+import com.ds.nodes.SingleNode;
+
+public class CircularSingleLinkedList<T> {
+
+	private SingleNode<T> head;
+	private SingleNode<T> tail;
+	private int size;
+
+	// exists linked list
+	public boolean existsLinkedList() {
+		return head != null;
+	}
+	
+	// add elements first or last
+	public void add(T value) {
+		SingleNode<T> node = new SingleNode<>();
+		node.setData(value);
+		// if size = 0 && linkedlist not exist
+		if(size == 0 && !existsLinkedList()) {  // set in head
+			head = new SingleNode<T>();			
+			node.setNext(node);   // for circularSLL
+			head = node;
+			tail = node;
+		} else {    // size != 0, then add at last/tail
+			node.setNext(head);  // for circularSLL
+			tail.setNext(node);
+			tail = node;
+		}
+		setSize(getSize()+1); // incrementing by 1
+	}
+	
+	// insert elements using index
+	public void add(int index, T value) {
+		// no linkedlist but index location is greater than 0
+		boolean isLL = existsLinkedList();
+		
+		SingleNode<T> node = new SingleNode<>();
+		node.setData(value);
+		
+		if(!isLL && (index>0 || index<0)) {
+			// throw error
+			throw new IndexOutOfBoundsException("Index: " + index + " Size: " + size);
+		} else if(!isLL && index == 0) {
+			head = new SingleNode<T>();
+			
+			node.setNext(node);  // for CSLL: null -> node
+			head = node;
+			tail = node;
+		} else {  // if linkedlist exists
+			if(index == 0) {   // insert at first position
+				node.setNext(head);
+				head = node;
+				
+				// update last elements next
+				tail.setNext(head);  // for CSLL
+			} 
+			else if(index >= size) {  // insert at last
+				node.setNext(head);  // for CSLL: null -> node
+				tail.setNext(node);
+				tail = node;
+			}
+			else {   // insert anywhere between first and last
+				SingleNode<T> tempNode = head;
+				int i = 0;
+				while(i < index - 1) {   // index-1: is need bcs we initially point to head which points to first element
+					tempNode = tempNode.getNext();
+					i++;
+				}
+				node.setNext(tempNode.getNext());
+				tempNode.setNext(node);
+			}
+		}
+		setSize(getSize()+1); // atlast to add by one for wherever one value is added
+	}
+	
+	// print all elements in linkedlist
+	public void traverseLinkedList() {
+		if(existsLinkedList()) {
+			SingleNode<T> tempNode = head;
+			for(int i=0; i<getSize(); i++) {
+				System.out.print(tempNode.getData());
+				if(i != getSize()-1) {
+					System.out.print(" -> ");
+				}
+				tempNode = tempNode.getNext();
+			}
+		} else {
+			System.out.println("LinkedList not exist!!!");
+		}
+		System.out.println();
+	}
+	
+	// print all elements in linkedlist twice to check circular connectivity
+		public void traverseLinkedListTwice() {
+			if(existsLinkedList()) {
+				SingleNode<T> tempNode = head;
+				for(int i=0; i<getSize()*2; i++) {
+					System.out.print(tempNode.getData());
+					if(i != getSize()*2-1) {
+						System.out.print(" -> ");
+					}
+					tempNode = tempNode.getNext();
+				}
+			} else {
+				System.out.println("LinkedList not exist!!!");
+			}
+			System.out.println();
+		}
+	
+	public boolean contains(T searchItem) {
+		if(existsLinkedList()) {
+			SingleNode<T> tempNode = head;
+			for(int i=0; i<getSize(); i++) {
+				if(searchItem.equals(tempNode.getData())) {
+					System.out.println("Found at location - " + i);
+					return true;
+				}
+				tempNode = tempNode.getNext();
+			}
+		}
+		System.out.println("Not found!!!");
+		return false;
+	}
+	
+	// delete last element
+	public void deleteNode() {
+		if(!existsLinkedList()) {
+			throw new IndexOutOfBoundsException("No linked list is created to delete");
+		} else {  // delete at last
+			SingleNode<T> tempNode = head;
+			for(int i=0; i<getSize()-1; i++) {
+				tempNode = tempNode.getNext();
+			}
+			if(tempNode == head) { // only one element in linkedlist
+				head = tail = null;
+				setSize(getSize()-1);
+				return;
+			}
+			tempNode.setNext(head);   // for CSLL -> null to head 
+			tail = tempNode;
+			setSize(getSize()-1);
+		}
+	}
+	
+	// delete specified element
+	public void deleteNode(int index) {
+		if(!existsLinkedList()) {
+			throw new IndexOutOfBoundsException("No linked list is created to delete");
+		}
+		else if(index == 0) {   // deletion of first node
+			head = head.getNext();
+			tail.setNext(head);   // for CSLL
+			setSize(getSize()-1);
+			if(getSize() == 0) {  // if it is only one element
+				tail = null;
+				head = null;   // for CSLL
+			}
+		}
+		else if(index >= getSize()) {    // deletion of last node
+			SingleNode<T> tempNode = head;
+			for(int i=0; i<getSize()-1; i++) {
+				tempNode = tempNode.getNext();
+			}
+			if(tempNode == head) {
+				head=tail=null;
+				setSize(getSize()-1);
+				return;
+			}
+			tempNode.setNext(head);    // for CSLL
+			tail = tempNode;
+			setSize(getSize()-1);
+		}
+		else {  // deletion at middle 
+			SingleNode<T> tempNode = head;
+			int i=0;
+			while(i < index-1) {
+				tempNode = tempNode.getNext();
+				i++;
+			}
+			tempNode.setNext(tempNode.getNext().getNext());
+			setSize(getSize()-1);
+		}
+	}
+	
+	public void deleteSLL() {
+		head = null;
+		tail = null;
+		System.out.println("Linked List Deleted");
+	}
+
+	public SingleNode<T> getHead() {
+		return head;
+	}
+
+	public void setHead(SingleNode<T> head) {
+		this.head = head;
+	}
+
+	public SingleNode<T> getTail() {
+		return tail;
+	}
+
+	public void setTail(SingleNode<T> tail) {
+		this.tail = tail;
+	}
+
+	public int getSize() {
+		return size;
+	}
+
+	public void setSize(int size) {
+		this.size = size;
+	}
+
+}
